56516fc6d0935ca16177ccc3a2926e95beb4b83f
Carlos Godoy <ct_godoy@outlook.com>
2019-10-06 13:10:01 -0500

    2D_Array_DS

---
+++ Console/2D_Array_ds/2D_Array_ds/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace _2D_Array_ds
+{
+    class Program
+    {
+        // Complete the hourglassSum function below.
+        static int hourglassSum(int[][] arr)
+        {
+            int figura;
+            int figuraMayor = 0;
+            List<int> sumas = new List<int>();
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    figura = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+                    sumas.Add(figura);
+                }
+            }
+
+            figuraMayor = sumas[0];
+            for (int i = 0; i < sumas.Count; i++)
+            {
+                if (figuraMayor < sumas[i])
+                {
+                    figuraMayor = sumas[i];
+                }
+            }
+
+            return figuraMayor;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@"C:\Users\shavo\2d_array.txt");
+
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = hourglassSum(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
